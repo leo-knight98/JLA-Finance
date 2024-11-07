025269589c2d6ce579a04ca08b042e591194ff1c
Josué Vilera <josuevilera.epet20@gmail.com>
2024-11-07 19:57:11 +0100

    pequeña mejora sidebar

---
+++ src/components/side-bar/Sidebar.tsx
@@ -0,0 +1,10 @@
+import styles from "./sidebar.module.css";
+function Sidebar() {
+    return (
+        <footer className={styles.sideBar}>
+            <h2>side bar</h2>
+        </footer>
+    );
+}
+
+export default Sidebar;
