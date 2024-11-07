561dde0365d467374b899f5e4c32c58201a38c46
Josué Vilera <josuevilera.epet20@gmail.com>
2024-11-07 19:53:39 +0100

    componentes principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Balance from "./components/balance/Balance";
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Balance />
+      <Sidebar />
     </>
   )
 }
